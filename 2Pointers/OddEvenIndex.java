a7c6026bff75249c85310374a004e059cc139b96
Jatin <jatin8872648396@gmail.com>
2026-02-19 17:21:06 +0530

    Leetcode922

---
+++ 2Pointers/OddEvenIndex.java
@@ -0,0 +1,25 @@
+public class OddEvenIndex {
+    public static int[] sortArrayByParityII(int[] nums) {
+         int n = nums.length;
+        int i = 0, j = 1;
+        while (i < n && j < n) {
+            if (nums[i] % 2 == 0) i += 2;
+            else if (nums[j] % 2 == 1) j += 2;
+            else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i += 2;
+                j += 2;
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {4,2,5,7};
+        sortArrayByParityII(nums);
+    }
+
+    
+}
