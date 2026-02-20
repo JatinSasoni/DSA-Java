e89359839417198a43d72eb8c99e8ae6aaa45a17
Jatin <jatin8872648396@gmail.com>
2026-02-20 10:20:00 +0530

    leetcode26

---
+++ 2Pointers/RemoveDuplicate.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicate {
+    public static int removeDuplicates(int[] nums) {
+        int i =0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){ //unique
+                nums[++i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+        int []arr= {1,1,2,2,3,4,4};
+        removeDuplicates(arr);
+    }
+}
