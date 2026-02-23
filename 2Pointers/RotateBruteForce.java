da40b6d2a5ede74a84e20c9ddc3375c6db71c0f9
Jatin <jatin8872648396@gmail.com>
2026-02-23 11:41:21 +0530

    brute

---
+++ 2Pointers/RotateBruteForce.java
@@ -0,0 +1,12 @@
+public class RotateBruteForce {
+    public void rotate(int[] nums, int k) {
+       int arr[]= new int[nums.length];
+       for(int i=0;i<nums.length;i++){
+            arr[(i+k)%nums.length]=nums[i];
+       }
+
+       for(int j=0;j<arr.length;j++){
+            nums[j]=arr[j];
+       }
+    }
+}
