a827e917654c3f6a1456fad9e2ab0517a9b9ab0e
Jatin <jatin8872648396@gmail.com>
2026-03-25 09:54:24 +0530

    maxSubbaryEasy

---
+++ PrefixSum/MaxSubarray.java
@@ -0,0 +1,20 @@
+package PrefixSum;
+
+public class MaxSubarray {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum =0;
+        int n = nums.length;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int max=sum;
+        for(int i=k;i<n;i++){
+            sum+=nums[i];
+            sum-=nums[i-k];
+            max = Math.max(max, sum);
+        }
+
+        return (double) max/k;
+
+    }
+}
