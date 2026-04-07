2cc00e98ed2790d4e182a001f95c73f9aa21c622
Jatin <jatin8872648396@gmail.com>
2026-04-07 20:30:35 +0530

    longest frequ

---
+++ SlidingWindow/LongestFreq.java
@@ -0,0 +1,20 @@
+public class LongestFreq {
+    public int maxFrequency(int[] nums, int k) {
+		Arrays.sort(nums);
+        int n = nums.length;
+        int j=0;
+        int ans=0;
+        long total=0;
+        
+        for(int i=0;i<n;i++){
+            total+=nums[i];
+
+            while((long)nums[i]*(i-j+1)-total > k){
+                total-=nums[j];
+                j++;
+            }
+            ans = Math.max(ans,i-j+1);
+        }
+        return ans;
+    }
+}
